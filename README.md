e252742da2b7b9da4c8b39fd22a6324f78e05249
chung3011 <chung.hitc97@gmail.com>
2021-12-08 22:24:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Emoji Picker
+
+* Install
+
+```shell
+npm i pick-emoji
+```
+
+* Getting Started
+
+In package.json file, add
+<pre><code>
+  "scripts": {
+    "pick-emoji": "node ./node_modules/pick-emoji/index.js"
+  }
+</code></pre>
+
+```shell
+npm run pick-emoji
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+const emoji = require('emoji.json');
+
+async function displayEmoji() {
+  const { AutoComplete } = require('enquirer');
+
+  const prompt = new AutoComplete({
+    name: 'emoji',
+    message: 'Pick emoji',
+    limit: 5,
+    choices: emoji.map((e) => ({ name: e.name, value: e.char })),
+  });
+
+  await prompt
+    .run()
+    .then((answer) => {
+      for (const e of emoji) {
+        e.name == answer && console.log(e.char);
+      }
+    })
+    .catch(console.error);
+}
+displayEmoji();
